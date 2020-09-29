36615a214873b07fec02adeb460d6bfe43ca808f
Luis Rayas <luis.reyes7482@alumnos.udg.mx>
2020-09-29 17:45:53 -0500

    Se agrego comparacion de tipos de dato

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 
 using namespace std;
+int tipoDato(string dato);
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string dato;
 
+    getline(cin, dato);
+    cout << tipoDato(dato);
     return 0;
+}
+
+int tipoDato(string dato){
+    if (dato == "int")
+    {
+        return sizeof(int);
+    }
+    if (dato == "char")
+    {
+        return sizeof(char);
+    }
+    if (dato == "short")
+    {
+        return sizeof(short);
+    }
+    if (dato == "float")
+    {
+        return sizeof(float);
+    }
+    if (dato == "double")
+    {
+        return sizeof(double);
+    }
+    if (dato == "long")
+    {
+        return sizeof(long);
+    }
+    return -1;
 }
